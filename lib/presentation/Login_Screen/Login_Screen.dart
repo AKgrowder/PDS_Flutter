a59ce9ea63ef4c5a05d9db593551a7ddbd1aa875
Archit Kheni (Growder) <130642277+AKgrowder@users.noreply.github.com>
2023-08-02 12:10:05 +0530

    login ui issue changes

--- lib/presentation/Login_Screen/Login_Screen.dart
+++ lib/presentation/Login_Screen/Login_Screen.dart
@@ -8,6 +8,7 @@ import 'package:archit_s_application1/API/Bloc/senMSG_Bloc/senMSG_cubit.dart';
 import 'package:archit_s_application1/core/app_export.dart';
 import 'package:archit_s_application1/core/utils/color_constant.dart';
 import 'package:archit_s_application1/core/utils/sharedPreferences.dart';
+import 'package:archit_s_application1/custom_bottom_bar/custom_bottom_bar.dart';
 import 'package:archit_s_application1/presentation/home/home.dart';
 import 'package:archit_s_application1/presentation/otp_verification_screen/otp_verification_screen.dart';
 import 'package:archit_s_application1/widgets/custom_elevated_button.dart';
@@ -101,7 +102,7 @@ class _LoginScreenState extends State<LoginScreen> {
                       BlocProvider<RegisterCubit>(
                         create: (context) => RegisterCubit(),
                       ),
-                    ], child: HomeScreen());
+                    ], child: BottombarPage(buttomIndex: 0,));
                   }));
                 }
 
@@ -133,7 +134,7 @@ class _LoginScreenState extends State<LoginScreen> {
                       BlocProvider<RegisterCubit>(
                         create: (context) => RegisterCubit(),
                       ),
-                    ], child: HomeScreen());
+                    ], child: BottombarPage(buttomIndex: 0,));
                   }));
                 } else {
                   Navigator.push(context, MaterialPageRoute(builder: (context) {
