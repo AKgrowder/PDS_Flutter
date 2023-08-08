63a72717a1992f2ede0dd3374014ff085c110c3c
Meet Lunagariya <meetlunagariya1999>
2023-08-08 16:51:36 +0530

    api and screens

---
+++ lib/API/Model/delete_room_model/Delete_room_model.dart
@@ -0,0 +1,21 @@
+class DeleteRoomModel {
+  String? message;
+  bool? object;
+  bool? success;
+
+  DeleteRoomModel({this.message, this.object, this.success});
+
+  DeleteRoomModel.fromJson(Map<String, dynamic> json) {
+    message = json['message'];
+    object = json['object'];
+    success = json['success'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['message'] = this.message;
+    data['object'] = this.object;
+    data['success'] = this.success;
+    return data;
+  }
+}
