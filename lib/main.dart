7d3aa719a0ad8c19b0db14c0c176bcf293a1a0f2
Archit Kheni (Growder) <130642277+AKgrowder@users.noreply.github.com>
2023-08-22 14:37:19 +0530

    - issue fix

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,6 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
-import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 
