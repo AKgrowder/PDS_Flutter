adc95e20b6b6432cdf4613397920ac554656bf43
Archit Kheni (Growder) <130642277+AKgrowder@users.noreply.github.com>
2023-08-24 19:09:24 +0530

    - ankur nu saf kari u

--- lib/API/Bloc/my_account_Bloc/my_account_cubit.dart
+++ lib/API/Bloc/my_account_Bloc/my_account_cubit.dart
@@ -2,21 +2,21 @@
 import 'package:archit_s_application1/API/Bloc/my_account_Bloc/my_account_state.dart';
 import 'package:archit_s_application1/API/Model/myaccountModel/myaccountModel.dart';
 import 'package:archit_s_application1/API/Repo/repository.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyAccountCubit extends Cubit<MyAccountState> {
   MyAccountCubit() : super(MyAccountInitialState()) {}
-  Future<void> MyAccount() async {
+  Future<void> MyAccount(BuildContext context) async {
+    dynamic myAccontDetails;
     try {
       emit(MyAccountLoadingState());
-      MyAccontDetails myAccontDetails = await Repository().myAccount();
+        myAccontDetails = await Repository().myAccount(    context);
       if (myAccontDetails.success == true) {
         emit(MyAccountLoadedState(myAccontDetails));
-      } else {
-        emit(MyAccountErrorState(myAccontDetails.message.toString()));
-      }
+      }  
     } catch (e) {
-      emit(MyAccountErrorState(e.toString()));
+      emit(MyAccountErrorState(myAccontDetails));
     }
   }
 
