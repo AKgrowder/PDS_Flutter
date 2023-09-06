13c39429a26817ce648848dbb1312979096bfde7
Meet Lunagariya <meetlunagariya1999>
2023-09-06 18:32:41 +0530

    PDS  issue changes

--- lib/API/Bloc/auth/otp_block.dart
+++ lib/API/Bloc/auth/otp_block.dart
@@ -1,9 +1,7 @@
-import 'package:pds/API/Bloc/auth/otp_state.dart';
-import 'package:pds/API/Model/authModel/getUserDetailsMdoel.dart';
-import 'package:pds/API/Model/otpmodel/otpmodel.dart';
-import 'package:pds/API/Repo/repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pds/API/Bloc/auth/otp_state.dart';
+import 'package:pds/API/Repo/repository.dart';
 
 class OtpCubit extends Cubit<OtpState> {
   OtpCubit() : super(OtpInitialState()) {}
@@ -15,10 +13,28 @@ class OtpCubit extends Cubit<OtpState> {
       otpModel = await Repository().otpModel(OTP, userNumber, context);
       if (otpModel.success == true) {
         emit(OtpLoadedState(otpModel));
+      }else{
+        emit(OtpErrorState(otpModel.message));
       }
     } catch (e) {
       print('LoginScreen-${e.toString()}');
       emit(OtpErrorState(otpModel));
+    }
+  }
+
+
+   Future<void> resendOtpApi(String userNumber, BuildContext context) async {
+    dynamic forgetpassword;
+    try {
+      emit(OtpLoadingState());
+      forgetpassword = await Repository().forgetpassword(userNumber, context);
+      if (forgetpassword.success == true) {
+        emit(resendOtpLoadedState(forgetpassword));
+      } else {
+        emit(OtpErrorState(forgetpassword.message));
+      }
+    } catch (e) {
+      emit(OtpErrorState(forgetpassword));
     }
   }
 }
