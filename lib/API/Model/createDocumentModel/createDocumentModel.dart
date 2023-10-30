e9132f74fd8eb7ac04f6e0a4fe7c8a6f6acc0248
BoradAnkur <ankur.borad@shishgroup.com>
2023-10-30 15:35:45 +0530

    api calling

--- lib/API/Model/createDocumentModel/createDocumentModel.dart
+++ lib/API/Model/createDocumentModel/createDocumentModel.dart
@@ -19,3 +19,25 @@ class ChooseDocument {
     return data;
   }
 }
+
+class ChooseDocument1 {
+  String? message;
+  String? object;
+  bool? success;
+
+  ChooseDocument1({this.message, this.object, this.success});
+
+  ChooseDocument1.fromJson(Map<String, dynamic> json) {
+    message = json['message'];
+    object = json['object'];
+    success = json['success'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['message'] = this.message;
+    data['object'] = this.object;
+    data['success'] = this.success;
+    return data;
+  }
+}
