75922a9749d2b21c3c03dc10eaa15fd213a64456
BoradAnkur <ankur.borad@shishgroup.com>
2023-12-19 18:11:17 +0530

    selectMultipleUsers_Chat in inbox

---
+++ lib/API/Model/selectMultipleUsers_ChatModel/selectMultipleUsers_ChatModel.dart
@@ -0,0 +1,21 @@
+class SelectMultipleUsersChatModel {
+  String? message;
+  String? object;
+  bool? success;
+
+  SelectMultipleUsersChatModel({this.message, this.object, this.success});
+
+  SelectMultipleUsersChatModel.fromJson(Map<String, dynamic> json) {
+    message = json['message'];
+    object = json['object'];
+    success = json['success'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['message'] = this.message;
+    data['object'] = this.object;
+    data['success'] = this.success;
+    return data;
+  }
+}
