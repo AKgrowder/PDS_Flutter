2d6ff5833cb50aeeb464a16175e6f8778f9e5179
BoradAnkur <ankur.borad@shishgroup.com>
2023-12-25 18:10:14 +0530

    working on sheet issue [200~

--- lib/presentation/ new/RePost_Screen.dart
+++ lib/presentation/ new/RePost_Screen.dart
@@ -201,15 +201,28 @@ class _RePostScreenState extends State<RePostScreen> {
         //   }
         // }
       }
-     if (state is SearchHistoryDataAddxtends) {
+      if (state is SearchHistoryDataAddxtends) {
         searchUserForInbox1 = state.searchUserForInbox;
         searchUserForInbox1?.object?.content?.forEach((element) {
           Map<String, dynamic> dataSetup = {
-            'id': element.userUid ,
-            'display': element.userName ,
+            'id': element.userUid,
+            'display': element.userName,
             'photo': element.userProfilePic
           };
+
           tageData.add(dataSetup);
+          List<Map<String, dynamic>> uniqueTageData = [];
+          Set<String> encounteredIds = Set<String>();
+          for (Map<String, dynamic> data in tageData) {
+            if (!encounteredIds.contains(data['id'])) {
+              // If the ID hasn't been encountered, add to the result list
+              uniqueTageData.add(data);
+
+              // Mark the ID as encountered
+              encounteredIds.add(data['id']);
+            }
+            tageData = uniqueTageData;
+          }
           if (tageData.isNotEmpty == true) {
             istageData = true;
           }
