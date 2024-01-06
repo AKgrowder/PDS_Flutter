509d4d0db68a41cc87a304cde927010e3e8f97fc
BoradAnkur <ankur.borad@shishgroup.com>
2024-01-06 15:26:35 +0530

    story pds sheet issue

--- stories_editor/lib/src/presentation/main_view/main_view.dart
+++ stories_editor/lib/src/presentation/main_view/main_view.dart
@@ -8,6 +8,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:stories_editor/src/domain/sevices/save_as_image.dart';
 import 'package:stories_editor/src/gallery_media_picker/gallery_media_picker.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:provider/provider.dart';
@@ -139,7 +140,7 @@ class _MainViewState extends State<MainView> {
   void dispose() {
     _controller?.pause();
     _controller?.dispose();
-    
+
     super.dispose();
   }
 
@@ -377,7 +378,7 @@ class _MainViewState extends State<MainView> {
 
                     /// bottom tools
                     if (!kIsWeb)
-                      BottomTools(
+                      /* BottomTools(
                         contentKey: contentKey,
                         onDone: (bytes) {
                           print("this is the new data set-$bytes");
@@ -390,7 +391,186 @@ class _MainViewState extends State<MainView> {
                         onDoneButtonStyle: widget.onDoneButtonStyle,
                         editorBackgroundColor: widget.editorBackgroundColor,
                         context1: context,
-                      ),
+                      ), */
+                      Container(
+                        decoration:
+                            const BoxDecoration(color: Colors.transparent),
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 30.w, vertical: 40.h),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              /// preview gallery
+                              Expanded(
+                                child: Container(
+                                  alignment: Alignment.centerLeft,
+                                  child: SizedBox(
+                                    child: _preViewContainer(
+                                      /// if [model.imagePath] is null/empty return preview image
+                                      child: controlNotifier.mediaPath.isEmpty
+                                          ? ClipRRect(
+                                              borderRadius:
+                                                  BorderRadius.circular(8),
+                                              child: GestureDetector(
+                                                onTap: () {
+                                                  controlNotifier.isDataGet =
+                                                      true;
+                                                  if (controlNotifier
+                                                      .mediaPath.isEmpty) {
+                                                    scrollProvider
+                                                        .pageController
+                                                        .animateToPage(0,
+                                                            duration:
+                                                                const Duration(
+                                                                    milliseconds:
+                                                                        300),
+                                                            curve:
+                                                                Curves.easeIn);
+                                                    /*  scrollNotifier.pageController
+                                          .animateToPage(1,
+                                              duration: const Duration(
+                                                  milliseconds: 300),
+                                              curve: Curves.ease); */
+                                                  }
+                                                },
+                                                child: const CoverThumbnail(
+                                                  thumbnailQuality: 150,
+                                                ),
+                                              ))
+
+                                          /// return clear [imagePath] provider
+                                          : GestureDetector(
+                                              onTap: () {
+                                                /// clear image url variable
+                                                controlNotifier.mediaPath = '';
+                                                itemProvider.draggableWidget
+                                                    .removeAt(0);
+                                                _controller?.dispose();
+                                              },
+                                              child: Container(
+                                                height: 45,
+                                                width: 45,
+                                                color: Colors.transparent,
+                                                child: Transform.scale(
+                                                  scale: 0.7,
+                                                  child: const Icon(
+                                                    Icons.delete,
+                                                    color: Colors.white,
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+
+                              /// center logo
+                              if (controlNotifier.middleBottomWidget != null)
+                                Expanded(
+                                  child: Center(
+                                    child: Container(
+                                        alignment: Alignment.bottomCenter,
+                                        child:
+                                            controlNotifier.middleBottomWidget),
+                                  ),
+                                ),
+
+                              /// save final image to gallery
+                              Expanded(
+                                child: Container(
+                                  alignment: Alignment.centerRight,
+                                  child: Transform.scale(
+                                    scale: 0.9,
+                                    child: AnimatedOnTapButton(
+                                        onTap: () async {
+                                          if (controlNotifier
+                                                  .mediaPath.isEmpty ==
+                                              true) {
+                                          } else {
+                                            // isData = false;
+
+                                            String pngUri;
+                                            print("check else condison");
+                                            await takePicture(
+                                                    isTextEditing:
+                                                        controlNotifier
+                                                            .isTextEditing,
+                                                    SelectPath: controlNotifier
+                                                        .mediaPath,
+                                                    contentKey: contentKey,
+                                                    context: context,
+                                                    saveToGallery: false)
+                                                .then((bytes) {
+                                              if (bytes != null) {
+                                                pngUri = bytes;
+                                                print(
+                                                    "asdfasdasdasdasdasdad-$pngUri");
+                                                print(pngUri);
+
+                                                print(pngUri);
+                                                    widget.onDone!(pngUri);
+
+                                              } else {}
+                                            });
+                                          }
+                                        },
+                                        child: Container(
+                                          padding: const EdgeInsets.only(
+                                              left: 12,
+                                              right: 5,
+                                              top: 4,
+                                              bottom: 4),
+                                          decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(15),
+                                              border: Border.all(
+                                                  color: isData == false &&
+                                                          controlNotifier
+                                                                  .mediaPath
+                                                                  .isEmpty ==
+                                                              true
+                                                      ? Colors.grey
+                                                      : Colors.white,
+                                                  width: 1.5)),
+                                          child: Row(
+                                              mainAxisSize: MainAxisSize.min,
+                                              children: [
+                                                Text(
+                                                  'Share',
+                                                  style: TextStyle(
+                                                      color: isData == false &&
+                                                              controlNotifier
+                                                                      .mediaPath
+                                                                      .isEmpty ==
+                                                                  true
+                                                          ? Colors.grey
+                                                          : Colors.white,
+                                                      letterSpacing: 1.5,
+                                                      fontSize: 16,
+                                                      fontWeight:
+                                                          FontWeight.w400),
+                                                ),
+                                                const Padding(
+                                                  padding:
+                                                      EdgeInsets.only(left: 5),
+                                                  child: Icon(
+                                                    Icons.arrow_forward_ios,
+                                                    color: Colors.white,
+                                                    size: 15,
+                                                  ),
+                                                ),
+                                              ]),
+                                        )),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
                   ],
                 ),
                 gallery: GalleryMediaPicker(
@@ -501,8 +681,6 @@ class _MainViewState extends State<MainView> {
                               border: Border.all(
                                 color: Colors.white,
                                 width: 1.2,
-
-
                               )),
                           child: const Text(
                             'Selected',
@@ -521,6 +699,17 @@ class _MainViewState extends State<MainView> {
           },
         ),
       ),
+    );
+  }
+
+  Widget _preViewContainer({child}) {
+    return Container(
+      height: 45,
+      width: 45,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(width: 1.4, color: Colors.white)),
+      child: child,
     );
   }
 
@@ -625,7 +814,6 @@ class _MainViewState extends State<MainView> {
           print("Done file Size 10 MB");
           controlNotifier.mediaPath = filepath;
           itemProvider.draggableWidget.insert(
-            
               0,
               EditableItem()
                 ..type = ItemType.video
